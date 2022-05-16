91fc8a329aa1d0788b791e0b65ba86da6706fe9b
Leutbing <spdhsrnvl123@naver.com>
2022-05-16 23:36:10 +0900

    five

--- index.js
+++ index.js
@@ -23,20 +23,20 @@
                 message_C: document.querySelector("#scroll-section-1 .message_C")
             },
             value: {
-                messageA_opacity_in: [0, 1, { start: 0, end: 0.1 }],
-                messageA_opacity_out: [1, 0, { start: 0.15, end: 0.2 }],
-                messageA_translateY_in: [30, 0, { start: 0, end: 0.1 }],
-                messageA_translateY_out: [0, -40, { start: 0.15, end: 0.2 }],
+                messageA_opacity_in: [0, 1, { start: 0, end: 0.16 }],
+                messageA_opacity_out: [1, 0, { start: 0.19, end: 0.26 }],
+                messageA_translateY_in: [30, 0, { start: 0, end: 0.15 }],
+                messageA_translateY_out: [0, -40, { start: 0.19, end: 0.26 }],
                 
-                messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
-                messageB_opacity_out: [1, 0, { start: 0.45, end: 0.5 }],
-                messageB_translateY_in: [30, 0, { start: 0.3, end: 0.4 }],
-                messageB_translateY_out: [0, -40, { start: 0.45, end: 0.5 }],
+                messageB_opacity_in: [0, 1, { start: 0.26, end: 0.42 }],
+                messageB_opacity_out: [1, 0, { start: 0.45, end: 0.52 }],
+                messageB_translateY_in: [30, 0, { start: 0.26, end: 0.41 }],
+                messageB_translateY_out: [0, -40, { start: 0.45, end: 0.52 }],
                 
-                messageC_opacity_in: [0, 1, { start: 0.6, end: 0.7 }],
-                messageC_opacity_out: [1, 0, { start: 0.75, end: 0.8 }],
-                messageC_translateY_in: [30, 0, { start: 0.6, end: 0.7 }],
-                messageC_translateY_out: [0, -450, { start: 0.75, end: 0.8 }],
+                messageC_opacity_in: [0, 1, { start: 0.52, end: 0.68 }],
+                messageC_opacity_out: [1, 0, { start: 0.71, end: 0.78 }],
+                messageC_translateY_in: [30, 0, { start: 0.52, end: 0.67 }],
+                messageC_translateY_out: [0, -450, { start: 0.71, end: 0.78 }],
 
             }
         },
@@ -101,21 +101,21 @@
                 // console.log('0 paly');
                 break;
             case 1:
-                if (scrollRatio <= 0.12) {
+                if (scrollRatio <= 0.17) {
                     scene[currentScene].objs.message_A.style.opacity = calculate(value.messageA_opacity_in, currentYOffset);                
                     scene[currentScene].objs.message_A.style.transform = `translateY(${calculate(value.messageA_translateY_in,currentYOffset)}px)`
                 } else {
                     scene[currentScene].objs.message_A.style.opacity = calculate(value.messageA_opacity_out, currentYOffset);                
                     scene[currentScene].objs.message_A.style.transform = `translateY(${calculate(value.messageA_translateY_out,currentYOffset)}px)`
                 }
-                if (scrollRatio <= 0.42) {
+                if (scrollRatio <= 0.43) {
                     scene[currentScene].objs.message_B.style.opacity = calculate(value.messageB_opacity_in, currentYOffset);
                     scene[currentScene].objs.message_B.style.transform = `translateY(${calculate(value.messageB_translateY_in,currentYOffset)}px)`
                 } else {
                     scene[currentScene].objs.message_B.style.opacity = calculate(value.messageB_opacity_out, currentYOffset);
                     scene[currentScene].objs.message_B.style.transform = `translateY(${calculate(value.messageB_translateY_out,currentYOffset)}px)`
                 }
-                if (scrollRatio <= 0.72) {
+                if (scrollRatio <= 0.69) {
                     scene[currentScene].objs.message_C.style.opacity = calculate(value.messageC_opacity_in, currentYOffset);
                     scene[currentScene].objs.message_C.style.transform = `translateY(${calculate(value.messageC_translateY_in,currentYOffset)}px)`
                 } else {
